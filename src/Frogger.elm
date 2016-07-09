a8db45c90622131ba878b9c3d0a1864f0ab4f86f
Herman verschooten <Herman@verschooten.net>
2016-07-09 12:16:19 +0200

    Initial commit

---
+++ src/Frogger.elm
@@ -0,0 +1,33 @@
+module Frogger exposing (..)
+
+import Html.App as App
+import Html exposing (..)
+
+type Msg
+ = NoOp
+
+type alias Model = Int
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    NoOp ->
+      (model, Cmd.none)
+
+view : Model -> Html Msg
+view model =
+  div [] [ text "Hello Frogger" ]
+
+main : Program Never
+main =
+  App.program
+    { init = (0, Cmd.none)
+    , update = update
+    , view = view
+    , subscriptions = subscriptions
+    }
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
